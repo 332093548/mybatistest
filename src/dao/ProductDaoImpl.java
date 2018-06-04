1bea8ef5ad69cceaba002687a9a07ba41bc8884b
Gua <332093548@qq.com>
2018-06-04 11:58:35 +0800

    完成了数据的更新操作,分析得知与插入代码几乎相同,而且事务需要手动声明(有很多横切代码),因此Mybatis一般会与Spring整合操作

--- src/dao/ProductDaoImpl.java
+++ src/dao/ProductDaoImpl.java
@@ -8,12 +8,36 @@ import model.Product;
 import mybatis.SqlSessionFactoryUtils;
 
 public class ProductDaoImpl {
-	
+
 	public static void main(String[] args) {
 		ProductDaoImpl daoImpl = new ProductDaoImpl();
 		Product product = new Product();
 		product.setName("yyy");
-		daoImpl.save(product);
+		product.setId(40);
+		product.setPrice(300.00);
+		product.setRemark("更新测试!");
+		daoImpl.update(product);
+	}
+
+	// mybatis实现数据入库的功能
+	public int update(Product product) {
+		// 1: 获取SqlSession
+		SqlSession sqlSession = null;
+		// sqlSesison默认是开启了事务,但是需要自己手动提交,而且出了异常需要回滚
+		try {
+			sqlSession = SqlSessionFactoryUtils.getSqlSession();
+			// 2: 执行相关操作,此处传入的参数,必须与parameterType="model.Product"相同
+			int count = sqlSession.update("aa.bb.cc.update", product);
+			// Integer.parseInt("abc");
+			sqlSession.commit(); // 手动提交(此处是编程式事务)
+			return count;
+		} catch (Exception e) {
+			sqlSession.rollback();
+			throw new RuntimeException(e);
+		} finally {
+			// 3: 释放资源
+			SqlSessionFactoryUtils.closeSession();
+		}
 	}
 
 	// mybatis实现数据入库的功能
@@ -25,13 +49,13 @@ public class ProductDaoImpl {
 			sqlSession = SqlSessionFactoryUtils.getSqlSession();
 			// 2: 执行相关操作,此处传入的参数,必须与parameterType="model.Product"相同
 			int count = sqlSession.insert("aa.bb.cc.save", product);
-//			Integer.parseInt("abc");
-			sqlSession.commit();   // 手动提交(此处是编程式事务)
+			// Integer.parseInt("abc");
+			sqlSession.commit(); // 手动提交(此处是编程式事务)
 			return count;
-		}catch (Exception e) {
+		} catch (Exception e) {
 			sqlSession.rollback();
 			throw new RuntimeException(e);
-		}finally {
+		} finally {
 			// 3: 释放资源
 			SqlSessionFactoryUtils.closeSession();
 		}
